f69d05aaa47f48a20807be85c5e284bd17a559b1
Tristan Fisher <code@tristanfisher.com>
2014-08-03 01:07:24 -0400

    removes test strings from main.py because they're in the readme and it's easier to code with the values for zulip being populated by env

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from plugin_zulip import IOBotZulip
 
+
 if __name__ == '__main__':
 
-    test_bot = IOBotZulip(bot_email='my_bot_email_address@example.org', bot_api_key='zulip_api_key')
+    test_bot = IOBotZulip()
     test_bot.callback(callback_type='message')
